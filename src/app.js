d9a61ab115462dcf25e2cadc34148594e02baa4f
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-04-26 02:06:21 +0400

    Add Swagger documentation for API

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import connect from "./database/mongo.js";
 import dotenv from "dotenv";
 import cors from "cors";
 import path from "path";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import yaml from "js-yaml";
 import { getAllTask } from "./controllers/Task-controller.js";
 import { addTask } from "./controllers/addTask-controller.js";
 import { toggleTask } from "./controllers/doneTask-controller.js";
@@ -17,13 +20,19 @@ app.use(express.json());
 
 app.use(cors());
 
+const fileContents = fs.readFileSync("./src/swagger.yaml", "utf8");
+
+const swaggerDocument = yaml.load(fileContents);
+
+const options = {
+  customSiteTitle: "ToDo APP - Swagger API",
+};
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
 app.use(
   express.static(path.join(new URL("../public", import.meta.url).pathname))
 );
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src/public/index.html"));
-});
 
 app.get("/api/projects", getAllTask);
 
